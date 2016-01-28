a52974a5fa3c66db4bfd732f0185ea3e33a2d087
Anasse EL-HANANI <ael-hana@e1r11p12.42.fr>
2016-01-28 06:38:58 +0100

    missing add way of list element

--- ft_lem-in.h
+++ ft_lem-in.h
@@ -1,3 +1,15 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lem-in.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-hana <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/28 00:45:43 by ael-hana          #+#    #+#             */
+/*   Updated: 2016/01/28 06:26:35 by ael-hana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include <stdlib.h>
 #include <unistd.h>
 #include "ft_printf.h"
@@ -9,6 +21,9 @@ typedef struct		s_lem_in
 	int				n_hans;
 	short			starttoend;
 	int				n_way;
-	struct s_lem-in	**way;
-	struct s_lem-in	*next;
+	struct s_lem_in	**way;
+	struct s_lem_in	*next;
 }					t_lem_in;
+void				*check_buf(char **tab);
+char				*ft_check_line(char *str);
+void				ft_error_lem_in(void);
--- lem-in.c
+++ lem-in.c
@@ -1,10 +1,16 @@
-#include "ft_lem-in.h"
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   lem-in.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-hana <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/28 00:38:31 by ael-hana          #+#    #+#             */
+/*   Updated: 2016/01/28 06:26:32 by ael-hana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
 
-void	ft_error_lem_in(void)
-{
-	ft_printf("ERROR");
-	exit(EXIT_FAILURE);
-}
+#include "ft_lem-in.h"
 
 char	**ft_read_stdin(void)
 {
@@ -27,27 +33,7 @@ char	**ft_read_stdin(void)
 		free(tmp);
 	}
 	ft_error();
-}
-
-void		check_diz(char **tab)
-{
-	int		i;
-	int		start;
-	int		end;
-
-	i = 0;
-	start = 0;
-	end = 0;
-	while (tab[i])
-	{
-		if (!ft_strcmp("##start", tab[i]))
-			++start;
-		if (!ft_strcmp("##end", tab[i]))
-			++end;
-		i++;
-	}
-	if (!(end == 1 && start == 1))
-		ft_error_lem_in();
+	return (NULL);
 }
 
 int			ft_return_digit(char *str)
@@ -61,30 +47,82 @@ int			ft_return_digit(char *str)
 	if ((i = ft_atoi(str)))
 		return (i);
 	ft_error_lem_in();
+	return (0);
 }
 
-t_lem_in	*ft_creat_map_lem(char **tab)
+t_lem_in	*ft_init_t_lem_in(char *name, void *next, short std, int hans)
 {
-	int	i;
-	int	nb_handes;
+	t_lem_in	*ptr;
+
+	if (!(ptr = malloc(sizeof(t_lem_in))))
+		ft_error_lem_in();
+	ptr->name = name;
+	ptr->next = next;
+	ptr->n_hans = hans;
+	ptr->starttoend = std;
+	ptr->n_way = 0;
+	ptr->way = NULL;
+	return (ptr);
+}
+
+t_lem_in		*ft_creat_map_lem(char **tab, char **name)
+{
+	int			i;
+	t_lem_in	*ptr;
+	int			osef;
+	int			len;
+	int			nb_handes;
 
 	i = 1;
+	ptr = NULL;
+	osef = 0;
 	nb_handes = ft_return_digit(tab[0]);
-	check_diz(tab);
-	while (tab[i++])
-		tab[i++];
-	return (NULL);
+	while (tab[i] && name[osef])
+	{
+		if (!ft_strcmp("##start", tab[i]))
+		{
+			++i;
+			ft_printf("start %s\n", name[osef]);
+			ptr = ft_init_t_lem_in(name[osef], ptr , 1, nb_handes);
+		}
+		else if (!ft_strcmp("##end", tab[i]))
+		{
+			++i;
+			ft_printf("end %s\n", name[osef]);
+			ptr = ft_init_t_lem_in(name[osef], ptr , 2, 0);
+		}
+		else
+		{
+			ft_printf("osef %s\n", name[osef]);
+			ptr = ft_init_t_lem_in(name[osef], ptr , 0, 0);
+		}
+		++osef;
+		++i;
+	}
+	return (ptr);
+}
+
+void		ft_print_list_lem(t_lem_in *ptr)
+{
+	while (ptr)
+	{
+		ft_printf("name : %s, nombre de fourmis %d, starttoend %d, n_way %d, way %s\n",
+				ptr->name,		ptr->n_hans,	ptr->starttoend, ptr->n_way, ptr->way);
+		ptr = ptr->next;
+	}
 }
 
 int		main(void)
 {
 	char	**tab;
+	void	*ptr;
 	int		i;
 
 	i = 1;
 	tab = ft_read_stdin();
+	ptr = ft_creat_map_lem(tab, ft_strsplit(check_buf(tab), '|'));
 	while (*tab)
 		ft_printf("line %d : %s\n", i++, *tab++);
-	
+	ft_print_list_lem(ptr);
 	return (0);
 }
