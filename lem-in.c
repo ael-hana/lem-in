e6610f579619bafba18691034e995e70402ad8db
Anasse EL-HANANI <ael-hana@e1r10p22.42.fr>
2016-01-30 03:55:00 +0100

    parsing maj

--- lem-in.c
+++ lem-in.c
@@ -6,7 +6,7 @@
 /*   By: ael-hana <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2016/01/28 00:38:31 by ael-hana          #+#    #+#             */
-/*   Updated: 2016/01/28 06:26:32 by ael-hana         ###   ########.fr       */
+/*   Updated: 2016/01/30 03:21:47 by ael-hana         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,7 +18,7 @@ char	**ft_read_stdin(void)
 	char	*new;
 	char	*tmp;
 	int		i;
-	char	len;
+	int		len;
 
 	i = 1;
 	new = ft_strdup("\0");
@@ -70,7 +70,6 @@ t_lem_in		*ft_creat_map_lem(char **tab, char **name)
 	int			i;
 	t_lem_in	*ptr;
 	int			osef;
-	int			len;
 	int			nb_handes;
 
 	i = 1;
@@ -101,8 +100,8 @@ void		ft_print_list_lem(t_lem_in *ptr)
 {
 	while (ptr)
 	{
-		ft_printf("name : %s, nombre de fourmis %d, starttoend %d, n_way %d, way %s\n",
-				ptr->name,		ptr->n_hans,	ptr->starttoend, ptr->n_way, ptr->way);
+		ft_printf("My ptr : %p name : %s, nombre de fourmis %d, starttoend %d, n_way %d, way %p\n",
+				ptr, ptr->name,		ptr->n_hans,	ptr->starttoend, ptr->n_way, ptr->way[0]);
 		ptr = ptr->next;
 	}
 }
@@ -114,41 +113,77 @@ t_lem_in	*find_list(t_lem_in *ptr, char *name)
 	return (ptr);
 }
 
-t_lem_in	**ft_realloc_tab(size_t size, t_lem_in **old, void *add)
+void		ft_realloc_tab(size_t size, t_lem_in *lst, void *add)
 {
 	t_lem_in		**tmp;
+	t_lem_in		**old;
 	unsigned int	i;
 
+	old = lst->way;
 	if (!(tmp = malloc(sizeof(t_lem_in) * (size + 1))))
 		ft_error_lem_in();
 	i = 0;
 	while (i < (size - 1))
-		tmp[i] = old[i++];
-	tmp[i++] = add;
-	tmp[i] = NULL;
-	free(old);
-	return (tmp);
+	{
+		tmp[i] = old[i];
+		if (tmp[i++] == add)
+			return ;
+	}
+	tmp[i] = add;
+	tmp[++i] = NULL;
+	if (old)
+		free(old);
+	lst->way = tmp;
+	lst->n_way = size;
 }
 
-void		ft_creat_way_lem_in(t_lem_in *ptr, char **tab)
+
+void		ft_creat_way_lem_in(t_lem_in *ptr, char **tab, int i)
 {
-	while ()
+	char	**tmp;
+	t_lem_in	*lst1;
+	t_lem_in	*lst2;
+
+	while (tab[i] && (ft_strchr(tab[i], '-') || (tab[i][0] == '#' && tab[i][1] != '#')))
+	{
+		if (tab[i][0] == '#' && tab[i][1] != '#')
+			++i;
+		else
+		{
+			tmp = ft_strsplit(tab[i], '-');
+			ft_putstr(tab[i]);
+			ft_putstr("\noklm1\n\n");
+			ft_putstr(tmp[0]);
+			ft_putstr("\noklm2\n\n");
+			if (!(lst1 = find_list(ptr, tmp[0])))
+				return ;
+			if (!(lst2 = find_list(ptr, tmp[1])))
+				return ;
+			ft_realloc_tab(lst1->n_way + 1, lst1, lst2);
+			ft_realloc_tab(lst2->n_way + 1, lst2, lst1);
+			++i;
+		}
+	}
 }
 
 int		main(void)
 {
 	char	**tab;
 	void	*ptr;
+	char	**tmp;
 	int		i;
 
 	i = 1;
 	tab = ft_read_stdin();
 	ptr = ft_creat_map_lem(tab, ft_strsplit(check_buf(tab), '|'));
+	tmp = tab;
 	while (*tab)
 		ft_printf("line %d : %s\n", i++, *tab++);
-	while (!ft_strchr(*tab, '-'))
-		(*tab)++;
-	ft_creat_way_lem_in(ptr, tab);
+	tab = tmp;
+	i = 0;
+	while (!ft_strchr(tab[i], '-'))
+		i++;
+	ft_creat_way_lem_in(ptr, tab, i);
 	ft_print_list_lem(ptr);
 	return (0);
 }
