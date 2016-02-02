ace9e768d890ccf4999c1926bae29d9e303b86f8
Anasse EL-HANANI <ael-hana@e1r11p19.42.fr>
2016-02-02 04:20:29 +0100

    add file test

---
+++ ft_creat_way_lem.c
@@ -0,0 +1,122 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_creat_way_lem.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-hana <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/02/01 18:57:15 by ael-hana          #+#    #+#             */
+/*   Updated: 2016/02/02 04:19:57 by ael-hana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_lem-in.h"
+
+t_lem_in	*ft_init_t_lem_in(char *name, void *next, short std, int hans)
+{
+	t_lem_in	*ptr;
+
+	if (!(ptr = malloc(sizeof(t_lem_in))))
+		ft_error_lem_in();
+	ptr->name = name;
+	ptr->next = next;
+	ptr->nw = 0;
+	ptr->vld = 0;
+	ptr->n_hans = hans;
+	ptr->starttoend = std;
+	ptr->n_way = 0;
+	ptr->way = NULL;
+	return (ptr);
+	}
+
+t_lem_in		*ft_creat_map_lem(char **tab, char **name)
+{
+	int			i;
+	t_lem_in	*ptr;
+	int			osef;
+	int			nb_handes;
+
+	i = 1;
+	ptr = NULL;
+	osef = 0;
+	nb_handes = ft_return_digit(tab[0]);
+	while (tab[i] && name[osef])
+	{
+		while (tab[i][0] == '#' && tab[i][1] != '#')
+			++i;
+		if (!ft_strcmp("##start", tab[i]))
+		{
+			++i;
+			ptr = ft_init_t_lem_in(name[osef], ptr , 1, nb_handes);
+		}
+		else if (!ft_strcmp("##end", tab[i]))
+		{
+			++i;
+			ptr = ft_init_t_lem_in(name[osef], ptr , 2, 0);
+		}
+		else
+			ptr = ft_init_t_lem_in(name[osef], ptr , 0, 0);
+		++osef;
+		++i;
+	}
+	return (ptr);
+}
+
+t_lem_in	*find_list(t_lem_in *ptr, char *name)
+{
+	while (ptr && ft_strcmp(ptr->name, name))
+		ptr = ptr->next;
+	return (ptr);
+}
+
+void		ft_realloc_tab(size_t size, t_lem_in *lst, void *add)
+{
+	t_lem_in		**tmp;
+	t_lem_in		**old;
+	unsigned int	i;
+
+	old = lst->way;
+	if (!(tmp = malloc(sizeof(t_lem_in) * (size + 1))))
+		ft_error_lem_in();
+	i = 0;
+	while (i < (size - 1))
+	{
+		tmp[i] = old[i];
+		if (tmp[i++] == add)
+			return ;
+	}
+	tmp[i] = add;
+	tmp[++i] = NULL;
+	if (old)
+		free(old);
+	lst->way = tmp;
+	lst->n_way = size;
+}
+
+void		ft_creat_way_lem_in(t_lem_in *ptr, char **tab, int i)
+{
+	char	**tmp;
+	t_lem_in	*lst1;
+	t_lem_in	*lst2;
+
+	while (tab[i] && (ft_strchr(tab[i], '-') || (tab[i][0] == '#' && tab[i][1] != '#')))
+	{
+		if (tab[i][0] == '#' && tab[i][1] != '#')
+			++i;
+		else
+		{
+			tmp = ft_strsplit(tab[i], '-');
+			ft_putstr(tab[i]);
+			ft_putstr("\noklm1\n\n");
+			ft_putstr(tmp[0]);
+			ft_putstr("\noklm2\n\n");
+			if (!(lst1 = find_list(ptr, tmp[0])))
+				return ;
+			if (!(lst2 = find_list(ptr, tmp[1])))
+				return ;
+			ft_realloc_tab(lst1->n_way + 1, lst1, lst2);
+			ft_realloc_tab(lst2->n_way + 1, lst2, lst1);
+			++i;
+		}
+	}
+}
---
+++ ft_parssing_lem_in.c
@@ -0,0 +1,82 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_parssing_lem_in.c                               :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: ael-hana <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2016/01/28 00:38:47 by ael-hana          #+#    #+#             */
+/*   Updated: 2016/02/02 04:19:56 by ael-hana         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "ft_lem-in.h"
+
+void	ft_error_lem_in(void)
+{
+	ft_printf("ERROR");
+	exit(EXIT_FAILURE);
+}
+
+char		*ft_check_line(char *str)
+{
+	char	**tab;
+	int		i;
+	int		len;
+
+	len = 0;
+	tab = ft_strsplit(str, ' ');
+	while (tab[len])
+		++len;
+	if (len != 3)
+		ft_error_lem_in();
+	i = 0;
+	while (tab[1][i] && ft_isdigit(tab[1][i++]));
+	if (tab[1][i])
+		ft_error_lem_in();
+	i = 0;
+	while (tab[2][i] && ft_isdigit(tab[2][i++]));
+	if (tab[2][i])
+		ft_error_lem_in();
+	return (ft_strjoin(tab[0], "|"));
+}
+
+void		*check_buf(char **tab)
+{
+	int		i;
+	int		start;
+	int		end;
+	char	*ret;
+
+	ret = "\0";
+	i = 1;
+	start = 0;
+	end = 0;
+	while (tab[i])
+	{
+		if (!ft_strcmp("##start", tab[i]))
+			++start;
+		else if (!ft_strcmp("##end", tab[i]))
+			++end;
+		else if (tab[i][0] != '#' && !ft_strchr(tab[i], '-'))
+			ret = ft_strjoin(ret, ft_check_line(tab[i]));
+		i++;
+	}
+	if (!(end == 1 && start == 1))
+		ft_error_lem_in();
+	return (ret);
+}
+
+int			ft_return_digit(char *str)
+{
+	int		i;
+
+	i = 0;
+	while (str[i] && ft_isdigit(str[i++]));
+	if (str[i])
+		ft_error_lem_in();
+	if ((i = ft_atoi(str)))
+		return (i);
+	ft_error_lem_in();
+	return (0);
+}
